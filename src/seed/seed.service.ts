7176aa4544bd22ab758fd14cada38ebbecc9d165
Deymer Hernandez <dhernant@contratista.epm.co>
2023-07-13 12:06:18 -0500

    Adapter pattern implemented using Axios

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,29 +1,29 @@
 import { Injectable } from '@nestjs/common';
-import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from 'src/interfaces/poke-response.interface';
-import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
+
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { AxiosAdapter } from '../common/adapters/axios.adapter';
 
 const URL = 'https://pokeapi.co/api/v2/pokemon?limit=650';
 
 @Injectable()
 export class SeedService {
 
-  private readonly axios: AxiosInstance = axios;
-
   constructor(
     @InjectModel(Pokemon.name)
-    private readonly pokemonModel: Model<Pokemon>) { }
+    private readonly pokemonModel: Model<Pokemon>,
+    private readonly httpAdapter: AxiosAdapter) { }
 
   async executeSeed() {
 
     await this.pokemonModel.deleteMany({});
 
-    const { data } = await this.axios.get<PokeResponse>(URL);
-    
+    const data = await this.httpAdapter.get<PokeResponse>(URL);
+
     const pokemonToInsert: { name: string, no: number }[] = [];
-    
+
     data.results.forEach(({ name, url }) => {
       const segements = url.split('/');
       const no = +segements[segements.length - 2];
